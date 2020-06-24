75139328e3093251a2fd395766d43a3bc79299a0
Yuliawan Rizka S <yuliawanrizka.syafaat@mitrais.com>
2020-06-24 14:59:34 +0700

    create controller file

---
+++ controller/todo.controller.js
@@ -0,0 +1,19 @@
+exports.todo_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: todo list');
+};
+
+exports.todo_create = function(req, res) {
+    res.send('NOT IMPLEMENTED: todo create');
+};
+
+exports.todo_get_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: todo get detail');
+};
+
+exports.todo_update = function(req, res) {
+    res.send('NOT IMPLEMENTED: todo update');
+};
+
+exports.todo_delete = function(req, res) {
+    res.send('NOT IMPLEMENTED: todo delete');
+};
--- routes/todo.js
+++ routes/todo.js
@@ -1,30 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var todoController = require('../controller/todo.controller');
 
 /* GET todo listing. */
-router.get('/', function(req, res) {
-  res.send('todo listing');
-});
+router.get('/', todoController.todo_list);
 
 /* POST todo create. */
-router.post('/', function(req, res) {
-  res.send('todo create');
-});
+router.post('/', todoController.todo_create);
 
 /* GET todo detail. */
-router.get('/:id', function(req, res) {
-  res.send('todo detail ' + req.params.id);
-});
+router.get('/:id', todoController.todo_get_detail);
 
 /* PUT todo update. */
-router.put('/:id', function(req, res) {
-  res.send('todo update ' + req.params.id);
-});
+router.put('/:id', todoController.todo_update);
 
 /* DELETE todo delete. */
-router.delete('/:id', function(req, res) {
-  res.send('todo delete ' + req.params.id);
-});
+router.delete('/:id', todoController.todo_delete);
 
 
 module.exports = router;
