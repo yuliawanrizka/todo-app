4a9b66a6d4f4c5e6775c36a54cd4d68620e44004
Yuliawan Rizka S <yuliawanrizka.syafaat@mitrais.com>
2020-06-25 12:17:54 +0700

    add swagger

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
+var expressSwagger = require('express-swagger-generator')(app);
+
+var swaggerOption = require('./config/expressOption.json');
 
 var indexRouter = require('./routes/index');
 var todoRouter = require('./routes/todo');
 
-var app = express();
+expressSwagger(swaggerOption);
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/todo.js
+++ routes/todo.js
@@ -2,19 +2,55 @@ var express = require('express');
 var router = express.Router();
 var todoController = require('../controller/todo.controller');
 
-/* GET todo listing. */
+/**
+ * Get all todo list
+ * @route GET /todo
+ * @group todo - Operations about todo
+ * @returns {object} 200 - An array of todo
+ * @returns {Error}  default - Unexpected error
+ */
 router.get('/', todoController.todo_list);
 
-/* POST todo create. */
+/**
+ * Create todo item
+ * @route POST /todo
+ * @group todo - Operations about todo
+ * @param {string} todo.query.required - todo text
+ * @returns {object} 200 - A created object of todo
+ * @returns {Error} 400 - No todo text specified
+ * @returns {Error}  default - Unexpected error
+ */
 router.post('/', todoController.todo_create);
 
-/* GET todo detail. */
+/**
+ * Get todo detail
+ * @route GET /todo/{id}
+ * @group todo - Operations about todo
+ * @param {string} id.path.required - id
+ * @returns {object} 200 - A created object of todo
+ * @returns {Error}  default - Unexpected error
+ */
 router.get('/:id', todoController.todo_get_detail);
 
-/* PUT todo update. */
+/**
+ * Update todo item
+ * @route PUT /todo/{id}
+ * @group todo - Operations about todo
+ * @param {string} id.path.required - id
+ * @param {string} todo.param.required - todo text
+ * @returns {object} 200 - A created object of todo
+ * @returns {Error}  default - Unexpected error
+ */
 router.put('/:id', todoController.todo_update);
 
-/* DELETE todo delete. */
+/**
+ * Delete todo item
+ * @route DELETE /todo/{id}
+ * @group todo - Operations about todo
+ * @param {string} id.path.required - id
+ * @returns {object} 200 - A created object of todo
+ * @returns {Error}  default - Unexpected error
+ */
 router.delete('/:id', todoController.todo_delete);
 
 
